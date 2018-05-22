7a74ca0feb8944b5a031f83ece4c97bb04525000
John Patrick Conway <JPatrick9793@gmail.com>
2018-05-22 13:37:43 -0400

    Added basic routes to aside link pages
    Simply returns string describing what the page will be

--- __init__.py
+++ __init__.py
@@ -7,8 +7,34 @@ from flask import render_template
 app = Flask(__name__) 
 
 @app.route('/') 
+@app.route('/') 
 def hello_world():
     return render_template('index.html')
+
+@app.route('/Resume')
+def getResumePage():
+    #return render_template('resume.html')
+    return ("This will be my resume page")
+
+@app.route('/Projects')
+def getResumePage():
+    #return render_template('resume.html')
+    return ("This will be my projects page")
+
+@app.route('/Skills')
+def getResumePage():
+    #return render_template('resume.html')
+    return ("This will be my skills page")
+
+@app.route('/About_me')
+def getResumePage():
+    #return render_template('resume.html')
+    return ('This will be my "About me" page')
+
+@app.route('/Contact_me')
+def getResumePage():
+    #return render_template('resume.html')
+    return ('This will be my "Contact Me" page')
 
 if __name__ == '__main__':
     app.run()
