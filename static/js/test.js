b744aba689c9b5f29c4cfef69cec83252512847b
John Patrick Conway <JPatrick9793@gmail.com>
2018-06-06 19:49:04 -0400

    add myFunction

--- static/js/test.js
+++ static/js/test.js
@@ -1,5 +1,25 @@
+function myFunction(txt) {
+    $.ajax({
+      type: "POST",
+      url: "http://www.thejohnconway.com/testing/sudoku",
+      data: txt,
+      cache: false,
+      dataType: "json",
+      // IF successful
+      success: function(data) {
+        alert("Success");
+        console.log(data)
+      },
+      // If unsuccessful
+      error: function(data) {
+        alert("There was an error");
+        console.log(data);
+      }
+    });
+}
+
+
 function alertSomething() {
-  
     $.ajax({
       type: "POST",
       url: "http://www.thejohnconway.com/testing/sudoku",
@@ -15,5 +35,4 @@ function alertSomething() {
         console.log(data);
       }
     });
-  
 }
