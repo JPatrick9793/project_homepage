b95ec91ab6450f1f3b908981a46f87de208a055f
John Patrick Conway <JPatrick9793@gmail.com>
2018-06-07 18:54:22 -0400

    Update test_react.js

--- static/js/test_react.js
+++ static/js/test_react.js
@@ -1,32 +1,12 @@
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	class Conway extends React.Component {
+		render() {
+			return (<h1>Greetings from John Conway</h1>);
+		}
+	}
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var Parent = function (_React$Component) {
-  _inherits(Parent, _React$Component);
-
-  function Parent() {
-    _classCallCheck(this, Parent);
-
-    return _possibleConstructorReturn(this, (Parent.__proto__ || Object.getPrototypeOf(Parent)).apply(this, arguments));
-  }
-
-  _createClass(Parent, [{
-    key: 'render',
-    value: function render() {
-      return React.createElement(
-        'div',
-        null,
-        'Something'
-      );
-    }
-  }]);
-
-  return Parent;
-}(React.Component);
-
-ReactDOM.render(React.createElement(Parent, null), document.getElementById('app'));
+	ReactDOM.render(
+		<Conway />,
+		document.getElementById('main')
+	);
